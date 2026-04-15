12177073fa13a4febe71de053a75f16a4fd9891b
ashurin10 <muhamadalfian01010@gmail.com>
2026-04-15 14:10:43 +0700

    chore: hapus file test/debug dan update dependency frontend

--- dump_menus.php
+++
@@ -1,7 +0,0 @@
-<?php
-require __DIR__.'/vendor/autoload.php';
-$app = require_once __DIR__.'/bootstrap/app.php';
-$app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-foreach(App\Models\Menu::all() as $m) {
-    echo $m->id . ' | ' . $m->name . ' | Parent: ' . $m->parent_id . ' | URL: ' . $m->url . PHP_EOL;
-}
